eae0d95982d9a8414a9fa6d812329f908adc503e
Emil Djupvik <emil14833@gmail.com>
2019-05-07 22:02:46 +0200

    added circles in info_route

--- lib/routes/info_route.dart
+++ lib/routes/info_route.dart
@@ -13,26 +13,73 @@ class InfoRoute extends StatefulWidget {
 }
 
 class _InfoRouteState extends State<InfoRoute> with TickerProviderStateMixin {
+  TabController _tabController;
+  final List<Widget> _tabs = [LearningRoute(), AboutRoute()];
+
+  @override
+  void initState() {
+    super.initState();
+    _tabController = TabController(length: 2, initialIndex: 0, vsync: this);
+    _tabController.addListener(() => setState(() {}));
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    _tabController.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
+    final double circleSize = 8;
+    final Color transparrent = Colors.white.withAlpha(100);
+
+    List<Widget> circles = [];
+    for (int i = 0; i < _tabs.length; i++) {
+      circles.add(
+        Container(
+          width: circleSize,
+          decoration: BoxDecoration(
+            shape: BoxShape.circle,
+            border: Border.all(color: i == _tabController.index ? Colors.white: transparrent),
+            color: i == _tabController.index ? Colors.white : Colors.transparent,
+          ),
+        ),
+      );
+      if (i >= _tabs.length - 1) continue;
+      circles.add(
+        SizedBox(width: circleSize * 1.5),
+      );
+    }
+
     return DefaultTabController(
-      length: 2,
-      child: Scaffold(
-        backgroundColor: Theme.of(context).backgroundColor,
-        floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
-        floatingActionButton: FloatingActionButton(
-          onPressed: widget.onBackPressed,
-          backgroundColor: Theme.of(context).accentColor,
-          child: Icon(Icons.arrow_downward),
-        ),
-        body: TabBarView(
-          controller: TabController(length: 2, initialIndex: 0, vsync: this),
-          children: <Widget>[
-            LearningRoute(),
-            AboutRoute(),
-          ],
-        ),
+      length: _tabs.length,
+      child: Stack(
+        alignment: Alignment.bottomCenter,
+        children: <Widget>[
+          Scaffold(
+            backgroundColor: Theme.of(context).backgroundColor,
+            floatingActionButtonLocation:
+                FloatingActionButtonLocation.centerFloat,
+            floatingActionButton: FloatingActionButton(
+              onPressed: widget.onBackPressed,
+              backgroundColor: Theme.of(context).accentColor,
+              child: Icon(Icons.arrow_downward),
+            ),
+            body: TabBarView(
+              controller: _tabController,
+              children: _tabs,
+            ),
+          ),
+          Container(
+            height: circleSize,
+            margin: EdgeInsets.only(bottom: 100),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: circles,
+            ),
+          ),
+        ],
       ),
     );
   }
