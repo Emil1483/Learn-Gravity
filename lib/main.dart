6afc92c142ab15863d9991cb73f51ff3aeb7afb5
Emil Djupvik <emil14833@gmail.com>
2019-05-09 20:27:46 +0200

    added a paragraph in learning_route

--- lib/main.dart
+++ lib/main.dart
@@ -37,6 +37,11 @@ class MyApp extends StatelessWidget {
               fontWeight: FontWeight.w400,
               color: Colors.white,
             ),
+            body1: TextStyle(
+              fontSize: 16.0,
+              color: Colors.white,
+              fontWeight: FontWeight.w200,
+            ),
           ),
         ),
         home: HomeRoute(),
--- lib/routes/learning_route.dart
+++ lib/routes/learning_route.dart
@@ -3,24 +3,30 @@ import 'package:flutter/material.dart';
 class LearningRoute extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    Widget title = Padding(
-      padding: EdgeInsets.all(16.0),
-      child: Text(
-        "How Does Gravity Work?",
-        style: Theme.of(context).textTheme.title,
-        textAlign: TextAlign.center,
-      ),
+    TextTheme textTheme = Theme.of(context).textTheme;
+
+    Widget title = Text(
+      "How Does Gravity Work?",
+      style: textTheme.title,
+      textAlign: TextAlign.center,
     );
 
     Widget gravityGif = Image.asset(
       "assets/elliptical_orbit.gif",
     );
 
+    Widget intro = Text(
+      "Gravity is an universal force that affects all parts of the Universe. It is the force that causes you stay on earth. It is the force that causes apples to fall. And it is the force that causes the moon to fall. But what do I mean with the moon falling? And what IS gravity and how does it work?",
+      style: textTheme.body1,
+      textAlign: TextAlign.center,
+    );
+
     return ListView(
-      padding: EdgeInsets.symmetric(horizontal: 24.0, vertical: 32.0),
+      padding: EdgeInsets.symmetric(horizontal: 40.0, vertical: 48.0),
       children: <Widget>[
         title,
         gravityGif,
+        intro,
       ],
     );
   }
