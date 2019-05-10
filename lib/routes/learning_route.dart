f981426fc9a86792445351562a95679d6d8e1c78
Emil Djupvik <emil14833@gmail.com>
2019-05-10 18:02:20 +0200

    added forceExplained in learningroute

--- lib/routes/learning_route.dart
+++ lib/routes/learning_route.dart
@@ -5,6 +5,8 @@ class LearningRoute extends StatelessWidget {
   Widget build(BuildContext context) {
     TextTheme textTheme = Theme.of(context).textTheme;
 
+    //TODO: Put all body text in a text tiles
+
     Widget title = Text(
       "How Does Gravity Work?",
       style: textTheme.title,
@@ -16,7 +18,7 @@ class LearningRoute extends StatelessWidget {
     );
 
     Widget intro = Text(
-      "Gravity is an universal force that affects all edges of the Universe. It is the force that causes you stay on earth. It is the force that causes apples to fall. And it is the force that causes the moon to fall. But what do I mean with the moon falling? And what IS gravity and how does it work?",
+      "Gravity is a universal force that affects all parts of the Universe. It is the force that causes you to stay on earth. It is the force that causes apples to fall. And it is the force that causes the moon to fall. But what do I mean with the moon falling? And what IS gravity and how does it work?",
       style: textTheme.body1,
       textAlign: TextAlign.center,
     );
@@ -27,18 +29,32 @@ class LearningRoute extends StatelessWidget {
       textAlign: TextAlign.center,
     );
 
+    Widget forceGif = Image.asset(
+      "assets/force_learning.gif",
+    );
+
+    Widget forceExplained = Text(
+      "Forces cause objects to accelerate, and when an object is accelerating, its velocity changes. Furthermore, a force contains a magnitude and a direction. Above, the yellow arrow’s length represents the force’s magnitude. And its direction represents the force’s direction.",
+      style: textTheme.body1,
+      textAlign: TextAlign.center,
+    );
+
     double width = MediaQuery.of(context).size.width;
     double maxWidth = 600;
     return ListView(
       padding: width <= maxWidth
           ? EdgeInsets.symmetric(horizontal: 40.0, vertical: 48.0)
-          : EdgeInsets.symmetric(horizontal: 40.0 + (width - maxWidth), vertical: 48.0),
+          : EdgeInsets.symmetric(
+              horizontal: 40.0 + (width - maxWidth), vertical: 48.0),
       children: <Widget>[
         title,
         gravityGif,
         intro,
         SizedBox(height: 16.0),
         introForce,
+        forceGif,
+        forceExplained,
+        SizedBox(height: 100.0),
       ],
     );
   }
